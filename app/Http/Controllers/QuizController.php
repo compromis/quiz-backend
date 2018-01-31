a4ffcf119c320ae23eaabfe082fadf7b9857ff7f
Guillem <guillem@disedit.com>
2018-01-31 16:28:11 +0100

    Add basic routes

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class QuizController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Add a score to the database
+     *
+     * @return Response
+     */
+    public function post_score($quiz)
+    {
+
+    }
+
+    /**
+     * Add individual quiz answers for statistical purposes
+     *
+     * @return Response
+     */
+    public function post_stats($quiz)
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->post('{quiz}/score', 'QuizController@post_score');
+$router->post('{quiz}/stat', 'QuizController@post_stat');
